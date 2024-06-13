b10481da341e74f435b10ec49d0bcda4e44ae17b
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-06-13 16:02:51 +0300

    initial

---
+++ practice5/DataBase/Mark.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace practice5;
+
+public partial class Mark
+{
+    public int Id { get; set; }
+
+    public string Subject { get; set; } = null!;
+
+    public int Mark1 { get; set; }
+    public string Semester { get; set; }
+}
---
+++ practice5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace practice5.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
